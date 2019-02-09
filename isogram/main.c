ffc46418c7321378ecfd8a2e9cdc3066455081a7
bBleck <bmbleck@hotmail.com>
2019-02-09 15:43:15 -0700

    Added functionality.

--- isogram/main.c
+++ isogram/main.c
@@ -1,3 +1,4 @@
+
 //
 //  main.c
 //  isogram
@@ -7,11 +8,49 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(int argc, const char * argv[]) {
-  
-  
-  
-  
+int is_isogram(char* inputString);
+int evalString(char* inputString);
+
+
+int main(int argc, char* argv[]) {
+  int i = 0;
+  if(argc ==2){
+    if(is_isogram(argv[1])){
+      i = 1;
+    }
+  }
+  if(i){
+    printf("%s is an isogram.\n", argv[1]);
+  }else{
+    printf("%s is not an isogram.]n", argv[1]);
+  }
   return 0;
+}
+
+int is_isogram(char* inputString){
+  if(inputString == NULL){
+    return 0;
+  }
+  if(strlen(inputString) == 0){
+    return 1;
+  }
+  return evalString(inputString);
+}
+
+int evalString(char* inputString){
+  int i = 0;
+  int j = 0;
+  for(i=0; i< (int)strlen(inputString); i++){
+    for(j=i+1; j< (int)strlen(inputString); j++){
+      if(tolower(inputString[i]) == tolower(inputString[j])){
+        if(isalpha(inputString[i])){
+          return 0;
+        }
+      }
+    }
+  }
+  return 1;
 }
